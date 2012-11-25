51d3e4ae7448c6f574f422c7166b10b7c04222fc
Yigal Korman <ykorman@gmail.com>
2012-11-25 20:17:52 +0200

    updated server side code

--- server_side.py
+++ server_side.py
@@ -1,4 +1,25 @@
 ﻿import sqlite3
 
 def get_player(name):
-    return 'aaa';
+    return Player(name);
+    
+def open_storage(name):
+    return Storage(name)
+
+class Player:   
+    def __init__(self, name=""):
+        self.name = name
+
+    def name(self):
+        return self.name
+        
+class Storage:
+    def __init__(self, name=":memory:"):
+        self.name = name
+        self.conn = sqlite3.connect(self.name)
+        
+    def __del__(self):
+        self.conn.close()
+        
+    def get_name(self):
+        return self.name
--- server_tests.py
+++ server_tests.py
@@ -3,8 +3,16 @@ import server_side
 
 class TestPlayerGet(unittest.TestCase):
     def testSimplePlayerGet(self):
-        p = server_side.get_player("player_name");
-        self.assertEqual("player_name", p.name);
+        p = server_side.get_player("player_name")
+        self.assertEqual("player_name", p.name)
+        p1 = server_side.get_player("player2")
+        self.assertEqual("player2", p1.name)
+        self.assertEqual("player_name", p.name)
+        
+class TestStorage(unittest.TestCase):
+    def testStorageOpen(self):
+        storage = server_side.open_storage("storage_name")
+        self.assertEqual(storage.get_name(), "storage_name")
     
 if __name__ == '__main__':
     unittest.main()
