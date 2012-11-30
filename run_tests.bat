f254103f1d487c57af0a50c088651dd1683c4d62
Yigal Korman <ykorman@gmail.com>
2012-11-30 20:44:07 +0200

    updated wesbserver sample

--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,7 @@
 @echo off
 set PATH=%PATH%;"C:\Program Files\BitNami DjangoStack\python"
 python.exe server_tests.py
-if ERRORLEVEL 0 python.exe webserver.py
+if ERRORLEVEL 0 start python.exe webserver.py
+rem echo Press any key to stop server
 rem pause
+rem taskkill /IM python.exe
